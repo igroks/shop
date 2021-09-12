0b5c8f73ba9757e00e99e13aa4d4628e05fcafc5
Igor Carvalho da Silva <igorcarvalhodasilva012@gmail.com>
2021-09-11 23:59:20 -0400

    Add import sintax

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require("express")
+import express from "express"
+
 const app = express()
+const PORT = 3000
 
 app.get("/", (req, res) => {
     res.end("Hello, World!")
 });
 
-app.listen(3000)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
