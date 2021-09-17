0c0cfb99850666b6d648b4b7fe42ea2e6e4ea7f1
Igor Carvalho da Silva <igorcarvalhodasilva012@gmail.com>
2021-09-17 00:20:34 -0400

    Add api models

---
+++ src/models/address.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Address extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Address.init({
+    userId: DataTypes.INTEGER,
+    street: DataTypes.STRING,
+    number: DataTypes.INTEGER,
+    neighborhood: DataTypes.STRING,
+    city: DataTypes.STRING,
+    uf: DataTypes.STRING,
+    cep: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Address',
+  });
+  return Address;
+};
