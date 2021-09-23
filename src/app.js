890c979cf058c3d314be8caa0aa0cf329f971ebd
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-09-23 00:58:55 -0400

    Add environment variables and set products router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express"
-import usersRoutes from "./routes/users"
+import usersRouter from "./routes/users"
+import productsRouter from "./routes/products"
+
+require("dotenv").config()
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.NODE_DOCKER_PORT;
 
 app.use(express.json());
-app.use("/users", usersRoutes);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
