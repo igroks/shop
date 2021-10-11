4aa98e375079e8247753808e0dd9fa39a26cc9c5
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-11 18:19:01 -0400

    Add swagger route

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
-import express from "express"
-import usersRouter from "./routes/users"
-import productsRouter from "./routes/products"
+import express from "express";
+import usersRouter from "./routes/users";;
+import productsRouter from "./routes/products";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../swagger.json";
 
-require("dotenv").config()
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.NODE_DOCKER_PORT;
 
+
 app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
