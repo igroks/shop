6bf6a29a49b8cf35400bd0b3217025433c23c598
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-13 13:27:25 -0400

    Add upload file route

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import express from "express";
-import usersRouter from "./routes/users";;
+import usersRouter from "./routes/users";
 import productsRouter from "./routes/products";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "../swagger.json";
+import multerRouter from "./routes/multer";
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.NODE_DOCKER_PORT;
 
-
 app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/upload-file", multerRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
