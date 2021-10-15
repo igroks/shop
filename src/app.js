a8e620a097858a1c1b6ab0ce2b207bbf21371c87
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-14 21:21:52 -0400

    Add cookie

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import usersRouter from "./routes/users";
 import productsRouter from "./routes/products";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "../swagger.json";
+import cookieParser from "cookie-parser";
 
 require("dotenv").config();
 
@@ -10,6 +11,19 @@ const app = express();
 const PORT = process.env.NODE_DOCKER_PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    if(!('unidade' in req.cookies)){
+        res.cookie('unidade', 'Instituto de Computação');
+        console.log('Created Cookie!');
+    }else{
+        console.log('There is already a cookie created!');
+        console.log(req.cookies);
+    }
+    next();
+})
+
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
