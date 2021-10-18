6991956f3fe73327fe8ccd0424a792159cb1ea39
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-18 12:05:00 -0400

    Add middleware to verify authentication

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,11 @@
 import express from "express";
 import productsController from "../controllers/products";
+import auth from "../utils/auth";
 
 const router = express.Router();
 
 router.get("/", productsController.index);
-router.post("/", productsController.create);
+router.post("/", auth.verifyAuth, productsController.create);
 router.get("/:id", productsController.read);
 router.post("/:id", productsController.update);
 router.delete("/:id", productsController.remove);
---
+++ src/utils/auth.js
@@ -0,0 +1,6 @@
+const verifyAuth = (req, res, next) => {
+    if(req.session.userId) next();
+    else res.status(401).json({msg: "Unauthorized"})
+}
+
+export default { verifyAuth }
