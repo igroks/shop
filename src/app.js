5bb9a98d765b5d15767cf7293aeedc419c88a74e
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-18 11:20:40 -0400

    Add sessions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import routes from "./routes";
+import { v4 as uuid } from "uuid";
+import session from "express-session";
 
 require("dotenv").config();
 
@@ -7,6 +9,16 @@ const app = express();
 const PORT = process.env.NODE_DOCKER_PORT;
 
 app.use(express.json());
+
+app.use(session({
+    genid: () => {
+        return uuid();
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { expires: 10000 }
+}));
 
 app.use(routes);
 
