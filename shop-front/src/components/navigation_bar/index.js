850ee7450afc30bbc65f3448758adea1de90a900
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-23 00:58:30 -0400

    Add a simple navigation bar

---
+++ shop-front/src/components/navigation_bar/index.js
@@ -0,0 +1,19 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const style = {
+    backgroundColor: "#212121"
+}
+
+export default function NavBar() {
+    return (
+        <AppBar position="static" style={style}>
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Shop
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+  }
