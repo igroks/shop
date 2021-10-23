0ccc2291993973dd7854e3d4b278a27723a294d5
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-23 19:03:21 -0400

    Continue the fix

--- shop-back/src/controllers/main.js
+++ shop-back/src/controllers/main.js
@@ -1,43 +1,43 @@
 import { User } from "../models/index";
 import bcrypt from "bcryptjs";
 
-const signUp = async(req, res) => {
-    try{
-        bcrypt.genSalt(parseInt(process.env.BCRYPT_ROUNDS), (error, salt) => {
-            bcrypt.hash(req.body.password, salt, async (error, hash) => {
-                await  User.create({...req.body, password: hash});
-                res.json({msg: "Created user"});
-            });
-        });
-    }catch (error){
-        res.status(500).json(error);
-    }
-}
+const signUp = async (req, res) => {
+  try {
+    bcrypt.genSalt(parseInt(process.env.BCRYPT_ROUNDS), (error, salt) => {
+      bcrypt.hash(req.body.password, salt, async (error, hash) => {
+        await User.create({ ...req.body, password: hash });
+        res.json({ msg: "Created user" });
+      });
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const login = async(req, res) => {
-    try{
-        const user = await User.findOne({ where: { email: req.body.email }});
-        if(user){
-            bcrypt.compare(req.body.password, user.password, (error, ok) => {
-                if(ok) {
-                    req.session.userId = user.id;
-                    res.json({msg: "Logged in user"});
-                }else{
-                    res.status(401).json({msg: "Incorrect password"});
-                }    
-            });
-        }else{
-            res.status(401).json({msg: "Incorrect email"});
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) {
+      bcrypt.compare(req.body.password, user.password, (error, ok) => {
+        if (ok) {
+          req.session.userId = user.id;
+          res.json({ msg: "Logged in user" });
+        } else {
+          res.status(401).json({ msg: "Incorrect password" });
         }
-    }catch (error){
-        res.status(500).json(error);
+      });
+    } else {
+      res.status(401).json({ msg: "Incorrect email" });
     }
-}
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const logout = async(req, res) => {
-    req.session.destroy(() => {
-        res.json({msg: "Session closed"});
-    });    
-}
+const logout = async (req, res) => {
+  req.session.destroy(() => {
+    res.json({ msg: "Session closed" });
+  });
+};
 
-export default { signUp, login, logout }
+export default { signUp, login, logout };
--- shop-back/src/controllers/products.js
+++ shop-back/src/controllers/products.js
@@ -3,85 +3,85 @@ import multer from "multer";
 import fs from "fs";
 
 const index = async (req, res) => {
-  try{
+  try {
     const products = await Product.findAll();
     res.send(products);
-  } catch (error){
+  } catch (error) {
     res.status(500).json(error);
   }
-}
+};
 
 const create = async (req, res) => {
-  try{
+  try {
     const product = await Product.create(req.body);
     res.send(product);
-  }catch (error){
+  } catch (error) {
     res.status(500).json(error);
   }
-}
+};
 
 const read = async (req, res) => {
-  try{
+  try {
     const { id } = req.params;
     const product = await Product.findByPk(id);
     if (product !== null) res.send(product);
-    else res.status(404).json({error: "Product not found"});
-  }catch (error){
+    else res.status(404).json({ error: "Product not found" });
+  } catch (error) {
     res.status(500).json(error);
   }
-}
+};
 
 const update = async (req, res) => {
-    try{
-        const { id } = req.params;
-        const [ found ] = await Product.update(req.body, {where:{id:id}});
-        if (found) res.status(200).json({ msg: "Updated product" });
-        else res.status(404).json({error: "Product not found"});
-    }catch (error){
-        res.status(500).json(error);
-    }
-}
+  try {
+    const { id } = req.params;
+    const [found] = await Product.update(req.body, { where: { id: id } });
+    if (found) res.status(200).json({ msg: "Updated product" });
+    else res.status(404).json({ error: "Product not found" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 const remove = async (req, res) => {
-    try{
-        const { id } = req.params;
-        const deleted = await Product.destroy({where:{id:id}});
-        if (deleted) res.status(200).json({ msg: "Deleted product" });
-        else res.status(404).json({error: "Product not found"});
-    }catch (error){
-        res.status(500).json(error);
-    }
-}
+  try {
+    const { id } = req.params;
+    const deleted = await Product.destroy({ where: { id: id } });
+    if (deleted) res.status(200).json({ msg: "Deleted product" });
+    else res.status(404).json({ error: "Product not found" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const { id } = req.params;
-        const dir = `public/uploads/${id}`;
-        if (!fs.existsSync(dir)) fs.mkdirSync(dir, {recursive: true});
-        cb(null, dir);
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    const { id } = req.params;
+    const dir = `public/uploads/${id}`;
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage }).single("file");
 
 const uploadFile = async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByPk(id);
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
 
-    if (product !== null){
-        upload(req, res, (error) =>{
-            if (error){
-                res.status(500).json(error);
-            }else{
-                res.json({msg: "File uploded"});
-            }
-        });
-    }else{
-        res.status(404).json({error: "Product not found"});
-    }
-}
+  if (product !== null) {
+    upload(req, res, (error) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        res.json({ msg: "File uploded" });
+      }
+    });
+  } else {
+    res.status(404).json({ error: "Product not found" });
+  }
+};
 
-export default { index, create, read, update, remove, uploadFile }
+export default { index, create, read, update, remove, uploadFile };
