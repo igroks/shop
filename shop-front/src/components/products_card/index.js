1695a93e84f27ead4f718f0bbc9a9411dc995122
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-23 22:29:16 -0400

    Add image in products card

--- shop-front/src/components/products_card/index.js
+++ shop-front/src/components/products_card/index.js
@@ -2,13 +2,25 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import CardMedia from '@mui/material/CardMedia';
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 function ProductCard(props) {
   return (
     <div>
-      <Card sx={{ minWidth: 345 }}>
+      <Card sx={{ 
+        minWidth: 345,
+        maxWidth: 345,
+        minHeight: 450,
+        maxHeight: 450  
+      }}>
+        <CardMedia
+          component="img"
+          height="340"
+          image="/static/images/products_card/box.jpg"
+          alt="product image"
+        />
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             {props.product_name}
