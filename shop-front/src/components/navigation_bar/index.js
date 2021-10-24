6f2a990449199f6ae3abd5c96196d030a01ef3a6
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-10-24 02:23:15 -0400

    Improve navigation bar code

--- shop-front/src/components/navigation_bar/index.js
+++ shop-front/src/components/navigation_bar/index.js
@@ -2,30 +2,31 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
 
 const style = {
   backgroundColor: "#212121",
 };
 
-const linksStyle = {
+const StyledLink = styled(Link)(() => ({
   color: "white",
   marginRight: 50,
-  textDecoration: "none",
-};
+  textDecoration: "none", 
+}));
 
 function NavBar() {
   return (
     <AppBar position="static" style={style}>
       <Toolbar>
         <Typography variant="h6" component="div">
-          <Link to="/" style={linksStyle}>
+          <StyledLink to="/">
             Shop
-          </Link>
+          </StyledLink>
         </Typography>
         <Typography variant="h9" component="div">
-          <Link to="/about" style={linksStyle}>
+          <StyledLink to="/about">
             Sobre
-          </Link>
+          </StyledLink>
         </Typography>
       </Toolbar>
     </AppBar>
