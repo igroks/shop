6d6348efbeb5cb417468697aef86d38cca65a165
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-11-17 10:57:30 -0400

    Add component to view product details

--- shop-front/src/components/product_view/index.js
+++ shop-front/src/components/product_view/index.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import Typography from "@mui/material/Typography";
-
+import IconButton from '@mui/material/IconButton';
+import Icon from '@mui/material/Icon';
 
 function ProductView() {
   const [product, setProduct] = useState({});
   const { id } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     fetch(`http://localhost:3020/products/${id}`, { credentials: "include" })
@@ -13,18 +15,23 @@ function ProductView() {
       .then((json) => setProduct(json));
   }, []);
 
+  const handleClick = () => {
+    history.goBack(-1);
+  }
+
   return(
     <div style={{display: "flex", justifyContent: "center"}}>
       <div style={{display: "flex", marginTop: 20, width: '65%', justifyContent: "space-between", height: "900px"}}>
-        <div style={{backgroundColor: "white", padding:"5px", width: "70%", borderRadius: 7}}>
-          <Typography sx={{fontSize: 32, fontWeight: "bold"}} color="black" gutterBottom>
+        <div style={{backgroundColor: "white", width: "70%", borderRadius: 7}}>
+          <IconButton size="small" style={{margin: "5px 5px 5px 5px"}} onClick={handleClick}><Icon>west</Icon></IconButton>
+          <Typography sx={{fontSize: 32, fontWeight: "bold", margin: "15px"}} color="black" gutterBottom>
             {product.name}
           </Typography>
-          <Typography sx={{fontSize: 16, fontWeight: "bold"}} color="text.secondary" gutterBottom>
+          <Typography sx={{fontSize: 16, fontWeight: "bold", margin: "15px"}} color="text.secondary" gutterBottom>
             {product.description}
           </Typography>
         </div>
-        <div style={{backgroundColor: "white", padding: "10px", width: "27.5%", borderRadius: 7}}>
+        <div style={{backgroundColor: "white", padding: "10px", width: "28%", borderRadius: 7}}>
           <Typography sx={{fontSize: 16, fontWeight: "bold"}} color="text.secondary" gutterBottom>
             {product.price}
           </Typography>
