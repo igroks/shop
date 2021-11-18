978783a20ed1a194e8cc19f21206d361218333ed
Igor Carvalho da Silva <igorcarvalho012@gmail.com>
2021-11-18 19:49:37 -0400

    Change margin to padding in products list and Add a floating button to add a new product

--- shop-front/src/App.js
+++ shop-front/src/App.js
@@ -1,6 +1,7 @@
 import NavBar from './components/navigation_bar';
 import ProductsList from './components/products_list';
 import ProductsView from './components/products_view';
+import ProductsAdd from './components/products_add';
 import About from './components/about';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
       <NavBar/>
       <Switch>
         <Route path="/" exact component={ProductsList}/>
+        <Route path="/products/add" exact component={ProductsAdd}/>
         <Route path="/products/:id" exact component={ProductsView}/>
         <Route path="/about" exact component={About}/>
       </Switch>
--- shop-front/src/components/products_list/index.js
+++ shop-front/src/components/products_list/index.js
@@ -39,7 +39,7 @@ function ProductsList() {
       <div style={{display: "flex", justifyContent: "center"}}>
         <SearchBar value={query} onChange={(e) => setQuery(e.target.value)}/>
       </div>
-      <div style={{ display: "flex", margin: "50px", flexWrap: 'wrap', justifyContent: "center"}}>
+      <div style={{ display: "flex", padding: "50px", flexWrap: 'wrap', justifyContent: "center"}}>
         {query === ""
           ? products.map((prod) => (
               <Link to={`/products/${prod.id}`} style={linksStyle}>
@@ -56,7 +56,7 @@ function ProductsList() {
               </Link>
             ))}
       </div>
-      <Fab color="primary" aria-label="add" onClick={handleClick}>
+      <Fab style={{position: 'fixed', top: '93%', right: '0.5%'}} color="primary" aria-label="add" onClick={handleClick}>
         <Icon>add</Icon>
       </Fab>
     </div>
